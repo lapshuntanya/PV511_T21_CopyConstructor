7f11a8995e060d20074084f287b33558978ce138
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-02 19:02:34 +0300

    class Medicine

---
+++ Medicine.h
@@ -0,0 +1,34 @@
+//
+// Created by Tetiana Perederii on 02.04.2026.
+//
+
+#ifndef PV511_T21_COPYCONSTRUCTOR_MEDICINE_H
+#define PV511_T21_COPYCONSTRUCTOR_MEDICINE_H
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Medicine {
+    char* title;
+    char* type;
+    float price;
+
+public:
+    Medicine();
+    Medicine(const char* title, const char* type, float price);
+    ~Medicine();
+
+    const char* getTitle()const;
+    const char* getType()const;
+    float getPrice()const;
+
+    void setTitle(const char* title);
+    void setType(const char* type);
+    void setPrice(float price);
+
+    void showInfo()const;
+};
+
+
+#endif //PV511_T21_COPYCONSTRUCTOR_MEDICINE_H
