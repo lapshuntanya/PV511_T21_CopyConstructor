118e57eb4fab13d9f2d68de35bc804dfa6ee936b
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-02 20:43:47 +0300

    class Medicine

--- Pharmacy.cpp
+++ Pharmacy.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "Pharmacy.h"
+
+Pharmacy::Pharmacy(const char *name) {
+    int len_name = strlen(name) + 1;
+    this->name = new char[len_name];
+    strcpy(this->name, name); // Clion
+    //strcpy_s(this->name, len_name, name); // VS
+
+    sizeMed = 0;
+    arrMed = nullptr;
+}
+
+Pharmacy::~Pharmacy() {
+    delete[] name;
+
+    if (arrMed != nullptr) {
+        delete[] arrMed;
+        arrMed = nullptr;
+    }
+}
+
+void Pharmacy::addMedicine(Medicine obj) {
+    addItemBack(arrMed, sizeMed, obj);
+}
+
+void Pharmacy::showPharm() const {
+    cout << "Pharmacy: " << name << endl;
+    cout << "Medicine: " << sizeMed << endl;
+    for (int i = 0; i < sizeMed; i++) {
+        cout << i+1 << ". " << arrMed[i].getTitle() << endl;
+        //arrMed[i].showInfo();
+    }
+}
+
+void Pharmacy::sortByPrice() {
+    mySort<Medicine>(arrMed, sizeMed, [](Medicine a, Medicine b) {
+        return b.getPrice() > a.getPrice();
+    });
+
+    showPharm();
+}
